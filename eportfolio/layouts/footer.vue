645557d269bf4f9df97f274d96ad354e01ac0fbb
Patrick Sabry <patrick.sabry@student.uts.edu.au>
2021-08-27 00:44:08 +1000

    reconcile bad commit

--- eportfolio/layouts/footer.vue
+++ eportfolio/layouts/footer.vue
@@ -63,19 +63,16 @@
       background-color: transparent;
       text-decoration: none;
     }
-
     a:visited {
       color: pink;
       background-color: transparent;
       text-decoration: none;
     }
-
     a:hover {
       color: red;
       background-color: transparent;
       text-decoration: underline;
     }
-
     a:active {
       color: yellow;
       background-color: transparent;
